5d3ca89a4b0d35d5e7b7059e44e46c0f8c5a3c6f
Golnar Sheikhshab <gsheikhshab@gsheikhshab01.phage.bcgsc.ca>
2016-11-23 16:31:46 -0800

    getting rid of all the datetime talk

--- Pipeline/Experiment_on_new_DataSet.sh
+++ Pipeline/Experiment_on_new_DataSet.sh
@@ -12,8 +12,8 @@ GENEFile=$7
 ALTGENEFile=$8
 
 
-echo "training CRF started at"
-date
+echo "training ..."
+#date
 
 cd ../banner/trunk
 ./scripts/banner.sh train $labelledConfigFile >& logfile
@@ -21,8 +21,8 @@ cd ../banner/trunk
 cd ../../Pipeline
 rm MiddleFiles/marginals.txt ; rm MiddleFiles/featureVectors.txt; rm MiddleFiles/sentencelengths.txt; rm MiddleFiles/transitions.txt; rm MiddleFiles/marginals_old.txt
 
-echo "labelling labelled set started at"
-date
+echo "putting reference labels ..."
+#date
 pwd
 cd ../banner/trunk
 pwd
@@ -30,26 +30,21 @@ pwd
 
 cd ../../Pipeline
 
-echo "Getting reference distributions started at"
-date
 
 ./Get_Ref_Distributions_For_Tokens.o 0 ../banner/trunk/output/training.txt MiddleFiles/featureVectors.txt MiddleFiles/Training_Token_Dists.txt 3 3 MiddleFiles/LabelsOrder.txt
 ./Get_Average_Dists.o 0 MiddleFiles/Training_Token_Dists.txt MiddleFiles/Ref_Dists_Averaged_For_Types 3 3
 
 
-echo "labelling unlabelled set started at"
-date
+echo "testing ..."
+#date
 
 rm MiddleFiles/marginals.txt ; rm MiddleFiles/featureVectors.txt; rm MiddleFiles/sentencelengths.txt; rm MiddleFiles/transitions.txt; rm MiddleFiles/marginals_old.txt
 pwd
 cd ../banner/trunk
 ./scripts/banner.sh test $unlabelledConfigFile >& logfile
 
-echo "mention.txt is being modified"
 ../../Pipeline/ModifyBannerMention.o 0 output/mention.txt output/mention_modified.txt
 
-echo "BioCreativeII Perl Evaluation:"
-date 
 
 cd ../../Data/bc2geneMention/train
 
@@ -58,19 +53,16 @@ echo "Kernel Performance:"
 perl alt_eval.perl -gene $GENEFile -altgene $ALTGENEFile ../../../banner/trunk/output/mention_modified.txt 
 
 perl alt_eval_detailedVersion.perl -gene $GENEFile -altgene $ALTGENEFile ../../../banner/trunk/output/mention_modified.txt > ../../../Pipeline/MiddleFiles/KernelDetailOutput
-echo "perl finished"
-date 
 
 cd ../../../Pipeline
 
-echo "GraphNER main steps started at"
 date
 
 ./Get_Average_Dists.o 0 MiddleFiles/marginals.txt MiddleFiles/Average_marginals.txt 3   3  
 ./test_viterbiDecoding.sh $alpha $mu $nu $itrNum $GENEFile $ALTGENEFile 
 
-echo "Experiment finished at"
-date
+echo "Done."
+
 
 
 
--- Pipeline/banner_chemdner_Experiment_on_new_DataSet.sh
+++ Pipeline/banner_chemdner_Experiment_on_new_DataSet.sh
@@ -12,8 +12,7 @@ GENEFile=$7
 ALTGENEFile=$8
 
 
-echo "training CRF started at"
-date
+echo "training ..."
 
 cd ../banner-chemdner/
 pwd
@@ -24,8 +23,7 @@ cd ../Pipeline
 pwd
 rm MiddleFiles/marginals.txt ; rm MiddleFiles/featureVectors.txt; rm MiddleFiles/sentencelengths.txt; rm MiddleFiles/transitions.txt rm MiddleFiles/marginals_old.txt
 
-echo "labelling labelled set started at"
-date
+echo "putting reference distributions on labelled vertices ... "
 
 cd ../banner-chemdner/
 pwd
@@ -34,26 +32,20 @@ ant -f build.xml run >& logfile
 
 cd ../Pipeline
 
-echo "Getting reference distributions started at"
-date
 ./Get_Ref_Distributions_For_Tokens.o 0 ../banner-chemdner/output/training.txt MiddleFiles/featureVectors.txt MiddleFiles/Training_Token_Dists.txt 3 3 MiddleFiles/LabelsOrder.txt
 ./Get_Average_Dists.o 0 MiddleFiles/Training_Token_Dists.txt MiddleFiles/Ref_Dists_Averaged_For_Types 3 3
 
 
 rm MiddleFiles/marginals.txt ; rm MiddleFiles/featureVectors.txt; rm MiddleFiles/sentencelengths.txt; rm MiddleFiles/transitions.txt rm MiddleFiles/marginals_old.txt
 
-echo "labelling unlabelled set started at"
-date
+echo "testing ..."
 
 cd ../banner-chemdner/
 echo "application.args=test "$unlabelledConfigFile > nbproject/project.applicationArgs
 ant -f build.xml run >& logfile
 
-echo "mention.txt is being modified"
 ../Pipeline/ModifyBannerMention.o 0 output/mention.txt output/mention_modified.txt
 
-echo "BioCreativeII Perl Evaluation:"
-date 
 
 cd ../Data/bc2geneMention/train
 pwd
@@ -63,11 +55,7 @@ perl alt_eval.perl -gene $GENEFile -altgene $ALTGENEFile  ../../../banner-chemdn
 
 perl alt_eval_detailedVersion.perl -gene $GENEFile -altgene $ALTGENEFile  ../../../banner-chemdner/output/mention_modified.txt > ../../../Pipeline/MiddleFiles/KernelDetailOutput
 
-echo "perl finished"
-date
 
-echo "GraphNER main steps started at"
-date
 
 cd ../../../Pipeline
 pwd
@@ -75,6 +63,6 @@ pwd
 ./banner_chemdner_test_viterbiDecoding.sh $alpha $mu $nu $itrNum $GENEFile $ALTGENEFile 
 
 
-echo "Experiment finished at"
+echo "Done."
 date
 
